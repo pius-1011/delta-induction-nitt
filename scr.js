629f1299ccdfaecd499d4b653ad1a01ad42566c2
pius-1011 <83703855+pius-1011@users.noreply.github.com>
2021-06-14 16:57:39 +0530

    Update scr.js

--- scr.js
+++ scr.js
@@ -54,7 +54,7 @@ class Grids {
         this.steps = 0;
         this.start_time = Date.now();
         this.running = true;
-        this.difficulty = 20;
+        this.difficulty = 30;
         this.score = 0;
 
         // initialize two 2d matrices 
